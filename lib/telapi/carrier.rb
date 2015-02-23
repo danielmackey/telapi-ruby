55138b7f9dc2169293b621a33060f313c27e5d69
Daniel Mackey <mackey@packagezen.com>
2015-02-23 15:58:52 -0600

    Provide list of previous carrier lookups

--- lib/telapi/carrier.rb
+++ lib/telapi/carrier.rb
@@ -3,6 +3,17 @@ module Telapi
   class Carrier < Resource
     class << self
 
+      # Returns a resource collection containing Telapi::Carrier objects
+      # See http://docs.telapi.com/v2/docs/carrier-lookup-list
+      #
+      # Optional params is a hash containing:
+      # +Page+:: integer greater than 0
+      # +PageSize+:: integer greater than 0
+      def list(optional_params = {})
+        response = Network.get(['Lookups', 'Carrier'], optional_params)
+        ResourceCollection.new(response, 'carrier_lookups', self)
+      end
+
       # Returns a Telapi::Carrier object given a phone number
       # See http://www.telapi.com/docs/api/rest/carrier-services/carrier-lookup/
       def lookup(phone_number)
